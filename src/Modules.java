f58bd82ac92f644cb0c9df9d6ce8e49593bc3c46
CezaryRJ <cezaryrj@ulrik.uio.no>
2018-08-13 01:22:00 +0200

    added file meta data extractor method

--- src/Modules.java
+++ src/Modules.java
@@ -3,15 +3,19 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.spi.FileTypeDetector;
+import java.util.ArrayList;
 
 public class Modules {
 
 	public static void main(String[] args) throws IOException {
 
 		System.out.println(compareFile(new File("test.txt"),new File("test1.txt")));
+		
+		getFileMetadata(new File("test.txt"));
 
 	}
-	
 	/***
 	 * 
 	 * @param file to be compared
@@ -50,6 +54,38 @@ public class Modules {
 			}
 		}
 		return((double)counter/(double)(largestSize));
+		
+	}
+	
+	/**
+	 * 
+	 * @param a file to be scanned
+	 * @return returns a list of meta file attributes
+	 * @throws IOException
+	 */
+	
+	public static ArrayList<String> getFileMetadata(File a ) throws IOException{
+		
+		Path file = Paths.get(a.getAbsolutePath());
+		BasicFileAttributes attr = Files.readAttributes(file, BasicFileAttributes.class);
+		
+		ArrayList<String> out = new ArrayList<>();
+		
+		out.add(a.getAbsolutePath().substring(a.getAbsolutePath().lastIndexOf('.'), a.getAbsolutePath().length()));
+		
+		out.add(attr.creationTime().toString());
+		out.add(attr.lastAccessTime().toString());
+		out.add(attr.lastModifiedTime().toString());
+
+		out.add(Long.toString(a.length()));
+		
+		/*
+		for(int i = 0; i<out.size();i++){
+			System.out.println(out.get(i));
+		}
+		*/
+		return out;
+		
 		
 	}
 
